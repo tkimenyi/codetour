5a14daa985b7c3c8f8492a3d75fdb149922c78a5
Thierry Kimenyi <tkimenyi@microsoft.com>
2023-02-27 18:47:26 -0800

    cleanup

--- src/player/index.ts
+++ src/player/index.ts
@@ -301,7 +301,7 @@ async function renderCurrentStep() {
       const lineAndFileInfoLabel =
         step.line && step.file
           ? `This step is on line ${step.line} in file ${step.file}`
-          : '';
+          : "";
 
       content = lineAndFileInfoLabel
         ? "\n\n---\n" +
@@ -309,7 +309,9 @@ async function renderCurrentStep() {
           "\n\n---\n" +
           content +
           "\n\n---\n"
-        : content;
+        : content + "\n\n---\n";
+    } else {
+      content += "\n\n---\n";
     }
 
     if (hasPreviousStep) {
